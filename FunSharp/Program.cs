97670ee30d69e549cc685847f63de1707f6af978
fzyier <babeckiy2004@gmail.com>
2022-10-14 17:47:59 +0300

    My First Scramble!

--- FunSharp/Program.cs
+++ FunSharp/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace FunSharp
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunSharp
 {
     class Program
     {
+        public static bool Scramble(string str1, string str2)
+        {
+            List<char> list = new List<char>(str2);
+            foreach (char c in str1)
+            {
+                list.Remove(c);
+            }
+
+            if (list.Count == 0)
+                return true;
+            else
+                return false;
+        }
         static void Main()
         {
+            Console.WriteLine(Scramble("rkqodlw", "world"));
+            Console.WriteLine(Scramble("cedewaraaossoqqyt", "codewars"));
+            Console.WriteLine(Scramble("katas", "steak"));
         }
     }
 }
